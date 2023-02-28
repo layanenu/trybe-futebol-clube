e2be534ac067b3dccb40ecfcd1119ee9b3370c32
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2023-02-28 20:12:55 -0300

    Requisito 1 e 2

---
+++ app/backend/src/database/migrations/20230228225650-User.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+module.exports = {
+ up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('users', {
+     id: {
+       allowNull: false,
+       autoIncrement: true,
+       primaryKey: true,
+       type: Sequelize.INTEGER
+     },
+     username: {
+       allowNull: false,
+       type: Sequelize.STRING,
+       field: 'username'
+     },
+     role: {
+       allowNull: false,
+       type: Sequelize.STRING,
+       field: 'role'
+     },
+     email: {
+       allowNull: false,
+       type: Sequelize.STRING,
+       field: 'email'
+     },
+     password: {
+       allowNull: false,
+       type: Sequelize.STRING,
+       field: 'password'
+   }
+   });
+ },
+
+
+ down: async (queryInterface, _) => {
+   await queryInterface.dropTable('users');
+ }
+};
+
+
+
